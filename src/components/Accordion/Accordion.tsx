90f74968bbdc53b03faf8056da9e1a70ebfde784
Khyati Bardolia <khyati.bardolia@gmail.com>
2023-09-21 12:43:12 -0400

    feat: create Accordion component ui, add its related svg icons

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, {ReactNode} from "react";
 import './Accordion.scss';
+import ArrowUpIcon from '../../assets/icons/arrow-up-icon.svg';
+import ArrowDownIcon from '../../assets/icons/arrow-down-icon.svg';
 
-export const Accordion: React.FC = () => {
+type Props = {
+    header: ReactNode;
+    content: ReactNode;
+    expanded: boolean;
+    onAccordionToggle: () => void;
+}
+
+export const Accordion: React.FC<Props> = ({header, content, expanded, onAccordionToggle}: Props) => {
     return <div className={'accordion-wrapper'}>
-        {/*Accordion*/}
+        <div className={'accordion-wrapper__header'} onClick={onAccordionToggle}>
+            {header}
+            <div className={'accordion-wrapper__header__expand'}>
+                <span>{!expanded ? 'Show' : 'Hide'}</span>
+                <img src={expanded ? ArrowUpIcon : ArrowDownIcon} alt={'arrow-icon'} />
+            </div>
+        </div>
+        {expanded && <div className={'accordion-wrapper__content'}>
+            {content}
+        </div>}
     </div>
 }
--- src/components/ProgressBarWidget/ProgressBarWidget.tsx
+++ src/components/ProgressBarWidget/ProgressBarWidget.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import './ProgressBarWidget.scss';
 import {ProgressBar} from "../ProgressBar/ProgressBar";
+import {Accordion} from "../Accordion/Accordion";
+import BookingFeaturesNotAllCheckedIcon from '../../assets/icons/booking-features-not-all-checked-icon.svg';
 
 type Props = {
     title: string;
@@ -8,14 +10,25 @@ type Props = {
 
 export const ProgressBarWidget: React.FC<Props> = ({title}: Props) => {
 
+    const onAccordionToggle = () => {}
+
     return <div className={'progress-bar-widget-wrapper'}>
         <div className={'progress-bar-widget-wrapper__header'}>
             <span className={'progress-bar-widget-wrapper__header__title'}>{title}</span>
-            <ProgressBar progressPercentage={"40"} />
+            <ProgressBar progressPercentage={"40"}/>
         </div>
 
         <div className={'progress-bar-widget-wrapper__content'}>
-            {/*Tasks list*/}
+            <Accordion
+                header={
+                    <div className={'progress-bar-widget-wrapper__accordion-header'}>
+                        <img src={BookingFeaturesNotAllCheckedIcon} alt={'clip-icon'}/>
+                        <span>title</span>
+                    </div>
+                }
+                content={<>content</>}
+                expanded={false}
+                onAccordionToggle={onAccordionToggle}/>
         </div>
     </div>
 }
